7b5be67a19555605ebeed82c594206025b455549
SNAN-RAZER <58069945+SNAN-RAZER@users.noreply.github.com>
2021-08-14 15:44:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
+var keys_included=[96,110,97,98,99,100,101,102,55,104,105,111,106,109,103,107];
 
-var data=""
 
-///Function to get only numbers
-function numbers(id){
-    const value=document.getElementById(id).textContent;
-    const display=document.getElementsByTagName('h3')[0];
-    data+= value;  
-    display.innerText=data;
-    
+function valueButton(e){
+    calculatorForm.screen.value += e.value;
+}
+function Clear(){
+    calculatorForm.screen.value = null;
+}
+function Calculate(){
+    calculatorForm.screen.value = eval(calculatorForm.screen.value);
+}
+window.addEventListener("keydown", checkKeyPress, false);
+function checkKeyPress(key) {
+    if  (keys_included.includes(key.keyCode) ) {
+        
+
+    }
+    else{
+        console.log(key.key)
+        alert("The " +key.key+" letter key has been pressed.");
+    }
 
 }
